11a5da2d4bfe2f6af7f66172c8e1da25a877d8ce
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 00:56:31 +0530

    Add files via upload

---
+++ pratical_2_3_c.py
@@ -0,0 +1,9 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-3.c.git
+#Aim:Write a Python program to create an intersection, Union, difference of sets.
+s={1,2,3,4}
+s1={4,3,5,6}
+print("union: ",s,s1)
+print("difference: ",s-s1)
+print("intersection: ",s&s1)
